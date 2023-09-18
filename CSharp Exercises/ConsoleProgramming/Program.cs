a14cd65874eee381d6272b06be5e61a2cc6b452e
Beliz Yazıcı <106441060+belizyazici@users.noreply.github.com>
2023-09-18 17:04:38 +0300

    Add files via upload

---
+++ CSharp Exercises/ConsoleProgramming/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ConsoleProgramming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           Console.WriteLine("İsminizi giriniz:");
+           string name = Console.ReadLine();
+
+           Console.WriteLine("Soyadınızı giriniz:");
+           string lastName = Console.ReadLine();
+
+           Console.WriteLine("Merhaba " + name + " " + lastName);
+        }
+    }
+    
+}
+
+
