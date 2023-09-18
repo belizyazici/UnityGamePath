5c9094f1afeb75ee8c78a2a43df03832cd7c6db4
Beliz Yazıcı <106441060+belizyazici@users.noreply.github.com>
2023-09-18 17:07:51 +0300

    Add files via upload

---
+++ CSharp Exercises/IfElseIf/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace IfElseIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("İyi günler");
+            }
+            else
+            {
+                Console.WriteLine("İyi geceler");
+            }
+
+            //ternary if ?: 
+            string sonuc = time <= 18 ? "İyi günler" : "İyi geceler";//18den küçükse ilkini yazdır değilse diğerini yazdır
+
+            sonuc = time >= 6 && time < 11 ? "Günaydın" : time <= 18 ? "İyi günler" : "İyi geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
