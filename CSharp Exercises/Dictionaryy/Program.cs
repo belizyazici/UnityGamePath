e39b6fa6c9a48e9c79853482b00fb480cfe0fb63
Beliz Yazıcı <106441060+belizyazici@users.noreply.github.com>
2023-09-19 22:47:44 +0300

    Add files via upload

---
+++ CSharp Exercises/Dictionaryy/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace Dictionaryy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> kullanicilar = new Dictionary<int, string>();
+
+            kullanicilar.Add(10, "Ayşe Yılmaz");
+            kullanicilar.Add(12, "Ahmet Yılmaz");
+            kullanicilar.Add(18, "Deniz Arda");
+            kullanicilar.Add(20, "Özcan Coşar");
+
+            //dizinin elemanlarına erişim
+            Console.WriteLine(kullanicilar[12]);
+            foreach(var item in kullanicilar)
+                Console.WriteLine(item);
+            
+            //Count
+            Console.WriteLine(kullanicilar.Count);
+
+            //contains
+            Console.WriteLine(kullanicilar.ContainsKey(12));
+            Console.WriteLine(kullanicilar.ContainsValue("Beliz"));
+
+            //remove
+            kullanicilar.Remove(12);
+            foreach(var item in kullanicilar)
+                Console.WriteLine(item.Value);
+
+            //keys
+            Console.WriteLine("***Keys***");
+            foreach(var item in kullanicilar.Keys)
+                Console.WriteLine(item);
+
+            //values
+            Console.WriteLine("***values***");
+            foreach(var item in kullanicilar.Values)
+                Console.WriteLine(item);
+        }
+    }
+}
