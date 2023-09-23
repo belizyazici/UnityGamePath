649a82458af5589b8ee7c7548c5be8ad04dd2270
Beliz Yazıcı <106441060+belizyazici@users.noreply.github.com>
2023-09-23 13:45:46 +0300

    Add files via upload

---
+++ CSharp Exercises/Collections-Question-3/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace CollectionsQuestion3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence: ");
+            string input = Console.ReadLine();
+
+            char[] vowels = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+
+            // Create a list to store vowels
+            var vowelList = new System.Collections.Generic.List<char>();
+
+            foreach (char c in input)
+            {
+                if (Array.Exists(vowels, v => v == char.ToLower(c)))
+                {
+                    vowelList.Add(c);
+                }
+            }
+
+            // Convert the list to an array
+            char[] vowelArray = vowelList.ToArray();
+
+            Console.WriteLine("Vowels in the sentence: " + string.Join(", ", vowelArray));
+        }
+    }
+}
